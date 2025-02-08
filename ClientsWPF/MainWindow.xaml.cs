f086a5764f407b99d16f36454dfe71314f6b5068
212201 <212201@1318-00011>
2025-02-08 11:03:59 +0300

    Добавьте файлы проекта.

---
+++ ClientsWPF/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ClientsWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void Refresh()
+        {
+            var users = await NetManager.Get<List<User>>("api/Users/GetAllUsers");
+            DGUsers.ItemsSource = users;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+        private async void BAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var user = new User();
+            user.Name = TBName.Text;
+            user.Age = int.Parse(TBAge.Text);
+            user.RoleId = CBRoles.SelectedIndex + 1;
+            await NetManager.Post("api/Users/Add", user);
+            Refresh();
+        }
+    } 
+}
---
+++ ClientsWPF/NetManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows.Markup;
+
+namespace ClientsWPF
+{
+    public static class NetManager
+    {
+        public static string URL = "http://localhost:55866/";
+        public static HttpClient httpClient = new HttpClient();
+        public static async Task<T> Get<T>(string controller)
+        {
+            var response = await httpClient.GetAsync(URL + controller);
+            var data = JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+            return data;
+        }
+        public static async Task<HttpResponseMessage> Post<T>(string controller, T data)
+        {
+            var jsonData = JsonConvert.SerializeObject(data);
+            var response = await httpClient.PostAsync(URL + controller, new StringContent(jsonData, Encoding.UTF8, "application/json"));
+            return response;
+        }
+        public static async Task<HttpResponseMessage> Put<T>(string controller, T data)
+        {
+            var jsonData = JsonConvert.SerializeObject(data);
+            var response = await httpClient.PutAsync(URL + controller, new StringContent(jsonData, Encoding.UTF8, "application/json"));
+            return response;
+        }
+        public static async Task<HttpResponseMessage> Delete<T>(string controller)
+        {
+            var response = await httpClient.DeleteAsync(URL + controller);
+            return response;
+        }
+    }
+}
